50b1e84f4a4cfeafa130a317134427acd032d3ca
PidgeyL <pieterjan.moreels@gmail.com>
2015-11-09 12:56:59 +0100

    bugfixes + compatibility problems

--- RHSA/redhatAdvisory.py
+++ RHSA/redhatAdvisory.py
@@ -30,49 +30,52 @@ def getContentOf(data, key, keyEncapsulation, valueEncapsulation):
   value = value[k+1:]
   return value.strip()
 
+def fetchURL(cve):
+  if (sys.version_info > (3, 0)):
+    return str(urllib.request.urlopen(url+cve).read(),"utf-8")
+  else:
+    return urllib.urlopen(url+cve).read()
+
 def main():
   description='''Queries the RedHat advisories to see if CVEs apply'''
   parser = argparse.ArgumentParser(description=description)
   parser.add_argument('cve', metavar='cve', type=str, help='CVEs to check against', nargs="?" )
   args = parser.parse_args()
 
+  # Get CVEs
   if args.cve:
     cves=[x.strip() for x in args.cve.split(",")]
   else:
     # Read from stdin
     cves=[]
     for line in fileinput.input(): cves.append(line.strip())
-
+  # Process CVEs
   try:
     for cve in cves:
       cve=cve.upper()
-      if re.match("CVE-\d\d\d\d-\d\d\d\d\d?", cve):
-        search=cve
-      elif re.match("\d\d\d\d-\d\d\d\d\d?", cve):
-        search="CVE-%s"%cve
-      elif re.match("\d\d\d\d\d?", cve):
-        search="CVE-%s-%s"%(date.today().year, cve)
+      # Check CVE & autofill
+      if re.match("CVE-\d\d\d\d-\d\d\d\d\d?", cve): search=cve
+      elif re.match("\d\d\d\d-\d\d\d\d\d?", cve):   search="CVE-%s"%cve
+      elif re.match("\d\d\d\d\d?", cve):            search="CVE-%s-%s"%(date.today().year, cve)
       else:
         print("Invalid CVE: " + cve)
         continue
+      # Fetch & parse
       try:
-        if (sys.version_info > (3, 0)):
-          f = str(urllib.request.urlopen(url+search).read(),"utf-8")
-        else:
-          f = urllib.urlopen(url+search).read()
-        if "<th>impact:</th>" in f.lower():
-          impact=getContentOf(f, "Impact:", "th", "td")
-          print("[+] %s - %s"%(search, impact))
-        elif "<dt>impact:</dt>" in f.lower():
-          impact=getContentOf(f, "Impact:", "dt", "span")
-          print("[+] %s - %s"%(search, impact))
-        if "<h2>statement</h2>" in f.lower():
-          print(" * %s"%getContentOf(f, "Statement", "h2", "p").replace("<br />", "\n * "))
-        elif '<h1>CVE not found</h1>' in f:
+        f = fetchURL(search)
+        if "<th>impact:</th>" in f.lower():         print("[+] %s - %s"%(search, getContentOf(f, "Impact:", "th", "td")))
+        elif "<dt>impact:</dt>" in f.lower():       print("[+] %s - %s"%(search, getContentOf(f, "Impact:", "dt", "span")))
+        # Statements
+        if "<h2>statement</h2>" in f.lower():       print(" * %s"%getContentOf(f, "Statement", "h2", "p").replace("<br />", "\n * "))
+        elif '<h1>cve not found</h1>' in f.lower(): print("[-] %s - N/A"%search)
+        elif '<title>page not found' in f.lower():  print("[-] %s - N/A"%search)
+      except urllib.error.HTTPError as err:
+        if err.code == 404:
           print("[-] %s - N/A"%search)
-      except KeyboardInterrupt:
-        sys.exit(0)
-  except IOError:
+        else:
+          print("Fetch error: %s"%err)
+  except KeyboardInterrupt:
+    print("Process interrupted by user")
+    sys.exit(0)
+  except IOError as e:
     print("Could not fetch the info. Are you connected to the Internet?")
-
-main()
